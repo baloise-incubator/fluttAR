299173b685fb886ddba235e5086cea4dcf35d25c
Lukas Brendle <lukas.a.j.brendle@gmail.com>
2020-09-21 16:33:51 +0200

    tabs in plugin code

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 import 'package:arkit_plugin/arkit_plugin.dart';
-import 'package:vector_math/vector_math_64.dart';
+import 'package:vector_math/vector_math_64.dart' as vector;
 
 void main() => runApp(MaterialApp(home: MyApp()));
 
@@ -11,6 +13,8 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   ARKitController arkitController;
+  String anchorId;
+  ARKitSphere sphere;
 
   @override
   void dispose() {
@@ -21,12 +25,67 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) => Scaffold(
       appBar: AppBar(title: const Text('ARKit in Flutter')),
-      body: ARKitSceneView(onARKitViewCreated: onARKitViewCreated));
+      body: ARKitSceneView(
+          onARKitViewCreated: onARKitViewCreated,
+          showFeaturePoints: true,
+          showWorldOrigin: true,
+          enableTapRecognizer: true,
+          planeDetection: ARPlaneDetection.horizontalAndVertical
+      ));
 
   void onARKitViewCreated(ARKitController arkitController) {
     this.arkitController = arkitController;
+    final material = ARKitMaterial(
+    diffuse: ARKitMaterialProperty(
+    color: Colors.yellow,
+    ));
+    sphere = ARKitSphere(
+      materials: [material],
+      radius: 0.1,
+    );
+
     final node = ARKitNode(
-        geometry: ARKitSphere(radius: 0.1), position: Vector3(0, 0, -0.5));
+      name: 'sphere',
+      geometry: sphere,
+      position: vector.Vector3(0, 0, -0.5),
+    );
+    this.arkitController.add(node);
+    //this.arkitController.onAddNodeForAnchor = _handleAddAnchor;
+    this.arkitController.onARTap = (results) => onTapHandler(results);
+    //this.arkitController.onNodeTap = (nodes) => onNodeTapHandler(nodes);
+  }
+
+  void _handleAddAnchor(ARKitAnchor anchor) {
+    if (anchor is ARKitPlaneAnchor) {
+      _addPlane(arkitController, anchor);
+    }
+  }
+
+  void _addPlane(ARKitController controller, ARKitPlaneAnchor anchor) {
+    anchorId = anchor.identifier;
+    final node =  ARKitNode(
+        geometry: ARKitSphere(radius: 0.1), position: vector.Vector3(0, 0, -0.5));
     this.arkitController.add(node);
   }
+
+  void onTapHandler(List<ARKitTestResult> results) {
+    final color = sphere.materials.value.first.diffuse.color == Colors.green
+        ? Colors.red
+        : Colors.green;
+    sphere.materials.value = [
+      ARKitMaterial(diffuse: ARKitMaterialProperty(color: color))
+    ];
+    log( "Tap found" + results.length.toString());
+    if(results.isEmpty){
+      return;
+    } else {
+      results.forEach((element) {
+         vector.Vector4 vector4 = element.worldTransform.getColumn(3);
+         final node =  ARKitNode(
+             geometry: ARKitSphere(radius: 0.1), position: vector.Vector3(vector4.x, vector4.y, vector4.z));
+         this.arkitController.add(node);
+      });
+    }
+  }
+
 }
