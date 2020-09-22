35577c0583cac6970595d88a5d0696cd8b2c6966
l0r5 <lars-braitinger@web.de>
2020-09-22 17:03:24 +0200

    Log alt, long, lat

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'dart:developer';
 
+import 'package:location/location.dart';
 import 'package:flutter/material.dart';
 import 'package:arkit_plugin/arkit_plugin.dart';
 import 'package:vector_math/vector_math_64.dart' as vector;
@@ -15,12 +16,16 @@ class _MyAppState extends State<MyApp> {
   ARKitController arkitController;
   String anchorId;
   ARKitSphere sphere;
+  double distance = 0;
+  List<String> nodeList;
 
   @override
   void dispose() {
     arkitController?.dispose();
     super.dispose();
   }
+
+
 
   @override
   Widget build(BuildContext context) => Scaffold(
@@ -55,18 +60,18 @@ class _MyAppState extends State<MyApp> {
     //this.arkitController.onNodeTap = (nodes) => onNodeTapHandler(nodes);
   }
 
-  void _handleAddAnchor(ARKitAnchor anchor) {
-    if (anchor is ARKitPlaneAnchor) {
-      _addPlane(arkitController, anchor);
-    }
-  }
-
-  void _addPlane(ARKitController controller, ARKitPlaneAnchor anchor) {
-    anchorId = anchor.identifier;
-    final node =  ARKitNode(
-        geometry: ARKitSphere(radius: 0.1), position: vector.Vector3(0, 0, -0.5));
-    this.arkitController.add(node);
-  }
+  // void _handleAddAnchor(ARKitAnchor anchor) {
+  //   if (anchor is ARKitPlaneAnchor) {
+  //     _addPlane(arkitController, anchor);
+  //   }
+  // }
+  //
+  // void _addPlane(ARKitController controller, ARKitPlaneAnchor anchor) {
+  //   anchorId = anchor.identifier;
+  //   final node =  ARKitNode(
+  //       geometry: ARKitSphere(radius: 0.1), position: vector.Vector3(0, 0, -0.5));
+  //   this.arkitController.add(node);
+  // }
 
   void onTapHandler(List<ARKitTestResult> results) {
     final color = sphere.materials.value.first.diffuse.color == Colors.green
@@ -79,13 +84,47 @@ class _MyAppState extends State<MyApp> {
     if(results.isEmpty){
       return;
     } else {
+      log("Hallo");
+
       results.forEach((element) {
-         vector.Vector4 vector4 = element.worldTransform.getColumn(3);
-         final node =  ARKitNode(
-             geometry: ARKitSphere(radius: 0.1), position: vector.Vector3(vector4.x, vector4.y, vector4.z));
-         this.arkitController.add(node);
+        vector.Vector4 vector4 = element.worldTransform.getColumn(3);
+        // print("Anchor Identifier:" + element.anchor.identifier);
+        // print("Anchor NodeName:" + element.anchor.nodeName);
+        // print("Distance: " + element.distance.toString());
+
+         final node =  ARKitNode(geometry: ARKitSphere(radius: 0.1), position: vector.Vector3(vector4.x, vector4.y, vector4.z));
+        log("Added Node to x - " + vector4.x.toString() + "y - " + vector4.y.toString() + "z - " + vector4.z.toString());
+        // this.nodeList.add(vector4.x.toString() +vector4.y.toString() + vector4.z.toString());
+        _getlocation();
+        this.arkitController.add(node);
       });
     }
   }
 
+  void _getlocation() async {
+    Location location = new Location();
+
+    bool _serviceEnabled;
+    PermissionStatus _permissionGranted;
+    LocationData _locationData;
+
+    _serviceEnabled = await location.serviceEnabled();
+    if (!_serviceEnabled) {
+      _serviceEnabled = await location.requestService();
+      if (!_serviceEnabled) {
+        return;
+      }
+    }
+
+    _permissionGranted = await location.hasPermission();
+    if (_permissionGranted == PermissionStatus.denied) {
+      _permissionGranted = await location.requestPermission();
+      if (_permissionGranted != PermissionStatus.granted) {
+        return;
+      }
+    }
+
+    _locationData = await location.getLocation();
+    log("alt: " + _locationData.altitude.toString() + " lat: " + _locationData.latitude.toString() + " long: " + _locationData.longitude.toString());
+  }
 }
