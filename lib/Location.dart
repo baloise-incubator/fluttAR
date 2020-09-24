feb596eae185d07be094778df4fad3d4abacd36a
Lukas Brendle <lukas.a.j.brendle@gmail.com>
2020-09-24 09:20:10 +0200

    saving data to firestore - v1 ( untested)

--- lib/Location.dart
+++ lib/Location.dart
@@ -1,3 +1,5 @@
+import 'dart:collection';
+
 class Location {
 
   double latitude;
@@ -8,6 +10,19 @@ class Location {
     this.latitude = latitude;
     this.longitude = longitude;
     this.meterOverNull = meterOverNull;
+  }
+
+  Map<String, dynamic> toDataStoreMap(){
+    var map = new HashMap<String, dynamic>();
+    var mapEntryLat = new MapEntry("latitude", latitude);
+    var mapEntryLong = new MapEntry("longitude", longitude);
+    var mapEntryAlt = new MapEntry("altitude", meterOverNull);
+
+    map.addEntries({mapEntryLat, mapEntryLong, mapEntryAlt});
+  }
+
+  String identifyingName(){
+    return latitude.toString() + "_" + longitude.toString() + "_" + meterOverNull.toString();
   }
 
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,24 @@
 import 'dart:developer';
 import 'dart:ffi';
 
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:fluttAR/Location.dart';
 import 'package:flutter/material.dart';
 import 'package:arkit_plugin/arkit_plugin.dart';
 import 'package:flutter/services.dart';
 import 'package:vector_math/vector_math_64.dart' as vector;
 
-void main() => runApp(MaterialApp(home: MyApp()));
+bool USE_FIRESTORE_EMULATOR = false;
+
+void main() async {
+  await Firebase.initializeApp();
+  if (USE_FIRESTORE_EMULATOR) {
+    FirebaseFirestore.instance.settings = Settings(
+        host: 'localhost:8080', sslEnabled: false, persistenceEnabled: false);
+  }
+  runApp(MaterialApp(home: MyApp()));
+}
 
 class MyApp extends StatefulWidget {
   @override
@@ -27,13 +38,14 @@ class _MyAppState extends State<MyApp> {
   }
 
   @override
-  Widget build(BuildContext context) => Scaffold(
-      appBar: AppBar(title: const Text('ARKit in Flutter')),
-      body: Container(
-          child: RaisedButton(
-        child: Text('start AR'),
-        onPressed: _startARSesssionAndSetLocations,
-      )));
+  Widget build(BuildContext context) =>
+      Scaffold(
+          appBar: AppBar(title: const Text('ARKit in Flutter')),
+          body: Container(
+              child: RaisedButton(
+                child: Text('start AR'),
+                onPressed: _startARSesssionAndSetLocations,
+              )));
 
   Future<void> _startARSesssionAndSetLocations() async {
     _startARSesssion();
@@ -41,15 +53,14 @@ class _MyAppState extends State<MyApp> {
 
     platform.setMethodCallHandler((MethodCall call) async {
       print("Flutter received call for" + call.method);
-      if(call.method == "dispatchLocation") {
+      if (call.method == "dispatchLocation") {
         print("Arguments received are : ${call.arguments}");
         var arguments = call.arguments;
-          Location location = new Location(
-              arguments[0], arguments[1], arguments[2]);
-          _persistToDB(location);
-        }
+        Location location = new Location(
+            arguments[0], arguments[1], arguments[2]);
+        _persistToDB(location);
+      }
     });
-
   }
 
   Future<void> _startARSesssion() async {
@@ -64,7 +75,8 @@ class _MyAppState extends State<MyApp> {
   Future<void> _transmitLocationInformation() async {
     try {
       Location loc = new Location(46.536671, 7.962324, 4158);
-      List<double> list = {loc.latitude, loc.longitude, loc.meterOverNull}.toList();
+      List<double> list = {loc.latitude, loc.longitude, loc.meterOverNull}
+          .toList();
       final int result = await platform.invokeMethod('setLocation', list);
       print("Location send");
     } on PlatformException catch (e) {
@@ -74,6 +86,18 @@ class _MyAppState extends State<MyApp> {
 
   Future<void> _persistToDB(Location location) async {
     print("Saving information");
+    var col =  FirebaseFirestore.instance
+        .collection("Locations").doc(location.identifyingName()).setData(location.toDataStoreMap());
+
+    /*
+    FirebaseFirestore
+        .instance
+        .collection("Locations")
+        .snapshots()
+        .listen((event) {
+      if (event.docChanges.)
+    })
+    */
   }
 
 }
